0d377909346dcaa7df850c3f8ddf88390cb2d2e8
Shaun S <shaunseeram@hotmail.com>
2023-11-16 22:34:09 -0500

    Organized code

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
-let notes = JSON.parse(localStorage.getItem("notes")) ?? [];
-
+// Define commonly used variables
 const filters = {
     text: "",
     sort: "dateA"
 }
 
 const monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
 const notesSection = document.querySelector(".notesListContainer");
-const filterInput = document.querySelector("#filter");
 const addNote = document.querySelector("#newNote");
 const editNoteTitle = document.querySelector("h2");
-const resetFormButton = document.querySelector("#resetForm");
-const sortInput = document.querySelector("#sorting");
 
+const resetAddNotes = () => {
+    editNoteTitle.textContent = "Add a Note";
+    addNote.reset();
+    addNote.dataset.editing = "";
+}
 
 const renderNotes = () => {
 
@@ -23,41 +23,29 @@ const renderNotes = () => {
 
         notes.sort((a, b) => {
             if (filters.sort === "dateA") {
-                if (a.date > b.date) {
-                    return 1
-                } else if (a.date < b.date) {
-                    return -1
-                } else {
-                    return 0
-                }
+                return a.date - b.date;
             } else if (filters.sort === "dateD") {
-                if (a.date > b.date) {
-                    return -1
-                } else if (a.date < b.date) {
-                    return 1
-                } else {
-                    return 0
-                }
+                return b.date - a.date;
             } else if (filters.sort === "titleA") {
-                if (a.title > b.title) {
-                    return 1
-                } else if (a.title < b.title) {
-                    return -1
+                if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                    return 1;
+                } else if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                    return -1;
                 } else {
-                    return 0
+                    return 0;
                 }
             } else if (filters.sort === "titleZ") {
-                if (a.title > b.title) {
-                    return -1
-                } else if (a.title < b.title) {
-                    return 1
+                if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                    return -1;
+                } else if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                    return 1;
                 } else {
-                    return 0
+                    return 0;
                 }
             }
         })
 
-        return notes.filter(note => note.title.toLowerCase().includes(filters.text.toLowerCase()) || note.body.toLowerCase().includes(filters.text.toLowerCase()))
+        return notes.filter(note => note.title.toLowerCase().includes(filters.text.toLowerCase()) || note.body.toLowerCase().includes(filters.text.toLowerCase()));
     }
 
     filteredNotes().forEach((note) => {
@@ -71,7 +59,6 @@ const renderNotes = () => {
         const editNote = document.createElement("button");
         const deleteNote = document.createElement("button");
 
-        container.setAttribute("data-id", note.date);
         container.classList.add("noteContainer");
 
         noteTitle.textContent = note.title;
@@ -80,30 +67,30 @@ const renderNotes = () => {
         noteText.textContent = note.body;
         noteText.classList.add("noteBody");
 
-        editNote.textContent = "Edit";
-        deleteNote.textContent = "Delete";
+        noteDate.textContent = `${monthArr[date.getMonth()]} ${date.getDay()}, ${date.getFullYear()}`;
 
+        editNote.textContent = "Edit";
         editNote.addEventListener("click", (e) => {
             addNote.noteTitle.value = note.title;
             addNote.noteBody.value = note.body;
             addNote.dataset.editing = note.date;
-            editNoteTitle.textContent = "Editing Note"
+            editNoteTitle.textContent = "Editing Note";
         })
 
+        deleteNote.textContent = "Delete";
         deleteNote.addEventListener("click", () => {
-            notes = notes.filter(indNote => indNote.date !== note.date)
-            localStorage.setItem("notes", JSON.stringify(notes))
+            notes = notes.filter(indNote => indNote.date !== note.date);
+            localStorage.setItem("notes", JSON.stringify(notes));
             renderNotes();
         })
 
+        noteFooterR.classList.add("noteFooterR");
         noteFooterR.appendChild(editNote);
         noteFooterR.appendChild(deleteNote);
-        noteFooterR.classList.add("noteFooterR")
 
-        noteDate.textContent = `${monthArr[date.getMonth()]} ${date.getDay()}, ${date.getFullYear()}`
+        noteFooter.classList.add("noteFooter");
         noteFooter.appendChild(noteDate);
         noteFooter.appendChild(noteFooterR);
-        noteFooter.classList.add("noteFooter")
 
         container.appendChild(noteTitle);
         container.appendChild(noteText);
@@ -113,27 +100,38 @@ const renderNotes = () => {
     })
 }
 
+// Adding note listeners
+
 addNote.addEventListener("submit", (e) => {
     e.preventDefault();
 
     if (e.target.dataset.editing !== "") {
-        const i = notes.findIndex(note => note.date === +e.target.dataset.editing)
+        const i = notes.findIndex(note => note.date === +e.target.dataset.editing);
         notes[i].title = e.target.noteTitle.value;
         notes[i].body = e.target.noteBody.value;
     } else {
         notes.push({
-            title: e.target.noteTitle.value,
+            title: e.target.noteTitle.value !== "" ? e.target.noteTitle.value : "Untitled Note",
             body: e.target.noteBody.value,
             date: new Date().getTime()
         })
     }
 
-    editNoteTitle.textContent = "Add a Note"
-    addNote.reset()
-    addNote.dataset.editing = "";
     localStorage.setItem("notes", JSON.stringify(notes));
+    resetAddNotes();
     renderNotes();
 })
+
+document.querySelector("#resetForm").addEventListener("click", resetAddNotes);
+
+// Filtering listeners
+
+document.querySelector("#sorting").addEventListener("change", (e) => {
+    filters.sort = e.target.value;
+    renderNotes();
+})
+
+const filterInput = document.querySelector("#filter");
 
 filterInput.addEventListener("input", (e) => {
     filters.text = e.target.value;
@@ -146,17 +144,10 @@ filterInput.addEventListener("keydown", (e) => {
     }
 })
 
-sortInput.addEventListener("change", (e) => {
-    filters.sort = e.target.value;
-    renderNotes();
-})
+// Initialize app
 
-resetFormButton.addEventListener("click", () => {
-    editNoteTitle.textContent = "Add a Note"
-    addNote.reset()
-    addNote.dataset.editing = "";
-})
-
-
+let notes = JSON.parse(localStorage.getItem("notes")) ?? [];
 document.querySelectorAll("form").forEach(form => form.reset());
 renderNotes();
+
+// Style. Add getter/setter later
